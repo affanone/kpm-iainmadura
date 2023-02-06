9f959a455daa49955ef0ecaabad58b714702ef52
affanone <affan.one@gmail.com>
2023-02-06 11:32:43 +0700

    a

--- routes/web.php
+++ routes/web.php
@@ -19,17 +19,13 @@ Route::get("/", function () {
     return view("welcome");
 });
 
-<<<<<<< HEAD
+Route::group(["prefix" => "dashboard"], function () {
+    Route::get("/", [DashboardController::class, "index"]);
+});
+
 Route::get("/signin", [AuthenticationController::class, "index"])->name(
     "login_page"
 );
 Route::post("/signin", [AuthenticationController::class, "login"])->name(
     "login_auth"
 );
-=======
-Route::get('/signin', [AuthenticationController::class, 'index'])->name('login_page');
-
-Route::group(["prefix" => "dashboard"], function () {
-    Route::get("/", [DashboardController::class, 'index']);
-});
->>>>>>> d7721fb5cd00363b14dfb82b0a91d4dddbb4502f
