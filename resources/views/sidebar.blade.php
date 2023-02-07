b694ce694708f64599b4349426a2ca773a5882d9
vathunt <fathan_46q12@ymail.com>
2023-02-07 16:06:05 +0700

    07 Februari 2023

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,10 +1,10 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-olive elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-        <img src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
-            class="brand-image img-circle elevation-3" style="opacity: 0.8" />
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+    <a href="#" class="brand-link">
+        <img src="http://iainmadura.ac.id/media/iainmadura.png" alt="Logo" class="brand-image img-circle elevation-3"
+            style="opacity: 0.8" />
+        <span class="brand-text font-weight-light">Aplikasi KPM</span>
     </a>
 
     <!-- Sidebar -->
